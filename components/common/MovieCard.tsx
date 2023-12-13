81b222fc7f7ad119432386cc28ee2c7a6da596fe
Duc <ducnhse20@gmail.com>
2023-12-13 12:01:54 +0700

    optimize image

--- components/common/MovieCard.tsx
+++ components/common/MovieCard.tsx
@@ -99,6 +99,8 @@ const MovieCard: React.FC<{ movieInfo: MovieModel }> = ({ movieInfo }) => {
         <img
           src={movieInfo?.poster}
           alt={movieInfo?.name}
+          width={"auto"}
+          height={"auto"}
           className="w-full h-[300px] object-cover hover:scale-105 transition-all ease-out"
         />
         <div className="absolute left-0 top-3 p-2 bg-red-500 rounded-r-md text-xs md:text-sm">
